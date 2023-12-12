f45daa40dbf1cb7bf683a5cadad81d0aab533639
Colligram <127895864+Colligram@users.noreply.github.com>
2023-12-12 11:00:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,53 +23,53 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  const form1 = document.querySelector("form");
+  // const form1 = document.querySelector("form");
 
-  // Getting the details from forms
-  const FullName = document.getElementById("recruiter_logname");
-  const phoneNumber = document.getElementById("recruiter_telephone");
-  const projectBudget = document.getElementById("budget");
-  const emailInput = document.getElementById("recruiter_logemail");
-  const messageSubject = document.getElementById("subject");
-  const Mess = document.getElementById("recruiter_textarea");
-  const friendUsername = document.getElementById("friend_logname");
-  const friendsMessage = document.getElementById("friend_textarea");
+  // // Getting the details from forms
+  // const FullName = document.getElementById("recruiter_logname");
+  // const phoneNumber = document.getElementById("recruiter_telephone");
+  // const projectBudget = document.getElementById("budget");
+  // const emailInput = document.getElementById("recruiter_logemail");
+  // const messageSubject = document.getElementById("subject");
+  // const Mess = document.getElementById("recruiter_textarea");
+  // const friendUsername = document.getElementById("friend_logname");
+  // const friendsMessage = document.getElementById("friend_textarea");
 
-  function sendEmail() {
-    const bodyMessage = `
-      Full Name: ${FullName.value}<br/>
-      Phone Number: ${phoneNumber.value}<br/>
-      Project Budget: ${projectBudget.value}<br/>
-      Email: ${emailInput.value}<br/>
-      Message Subject: ${messageSubject.value}<br/>
-      Message: ${Mess.value}<br/>
-      Friend Username: ${friendUsername.value}<br/>
-      Friend's Message: ${friendsMessage.value}
-    `;
+  // function sendEmail() {
+  //   const bodyMessage = `
+  //     Full Name: ${FullName.value}<br/>
+  //     Phone Number: ${phoneNumber.value}<br/>
+  //     Project Budget: ${projectBudget.value}<br/>
+  //     Email: ${emailInput.value}<br/>
+  //     Message Subject: ${messageSubject.value}<br/>
+  //     Message: ${Mess.value}<br/>
+  //     Friend Username: ${friendUsername.value}<br/>
+  //     Friend's Message: ${friendsMessage.value}
+  //   `;
 
-    Email.send({
-      Host: "smtp.elasticemail.com",
-      Username: "locozoombady@gmail.com",
-      Password: "CF046471BB200F8E23F21B9C19821EA7FF77",
-      To: "locozoombady@gmail.com",
-      From: "locozoombady@gmail.com",
-      Subject: messageSubject.value,
-      Body: bodyMessage,
-    }).then((message) => {
-      if (message === "OK") {
-        Swal.fire({
-          title: "SUCCESS!",
-          text: "Your message sent to collins successfully!",
-          icon: "success",
-        });
-      }
-    });
-  }
+  //   Email.send({
+  //     Host: "smtp.elasticemail.com",
+  //     Username: "locozoombady@gmail.com",
+  //     Password: "CF046471BB200F8E23F21B9C19821EA7FF77",
+  //     To: "locozoombady@gmail.com",
+  //     From: "locozoombady@gmail.com",
+  //     Subject: messageSubject.value,
+  //     Body: bodyMessage,
+  //   }).then((message) => {
+  //     if (message === "OK") {
+  //       Swal.fire({
+  //         title: "SUCCESS!",
+  //         text: "Your message sent to collins successfully!",
+  //         icon: "success",
+  //       });
+  //     }
+  //   });
+  // }
 
-  form1.addEventListener("submit", (e) => {
-    e.preventDefault();
-    sendEmail();
-  });
+  // form1.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   sendEmail();
+  // });
 
   function coffee() {
     alert("Your support means the world! By buying me a coffee, you're helping fuel late-night coding sessions. Click OK to treat me to a virtual coffee and support my work!");
